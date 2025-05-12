48e6a20640fdb6d1b28b17c5c4fe393441e4a62c
leavethetrain <musik201092@web.de>
2025-05-12 21:29:35 +0200

    add deleteAll Button/CSS styling

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const notesListEL = document.querySelector(".note-list");
 const saveButtonEl = document.querySelector(".save");
+const titleAndInputEl = document.querySelector(".main-content");
 const titleInputEl = document.getElementById("title-input-id");
 const noteInputEl = document.getElementById("note-content-id");
 const addButtonEl = document.getElementById("add-note-id");
 const deleteButtonEl = document.getElementById("delete-button");
-const searchInputEl = document.getElementById("search-input")
+const searchInputEl = document.getElementById("search-input");
 const searchButtonEl = document.querySelector(".search-button");
-
+const sidebarEL = document.querySelector(".sidebar");
+const cancelButtonEl = document.querySelector(".quit");
+const deleteAllButtonEl = document.querySelector(".delete-all");
+const emptyEl = document.querySelector(".empty");
 
 saveButtonEl.addEventListener("click", clickSaveButton);
 addButtonEl.addEventListener("click", newNote);
 deleteButtonEl.addEventListener("click", deleteNote);
 searchButtonEl.addEventListener("click", searchNotes);
 searchInputEl.addEventListener("keydown", searchNotes);
+cancelButtonEl.addEventListener("click", cancelButton);
+deleteAllButtonEl.addEventListener("click", deletAll);
 
 createNote();
 applyListeners();
@@ -34,7 +40,7 @@ function createNote(filteredInput = null) {
 
   sortedNotes.forEach((note) => {
     sidebar += `
-    
+    <div class="note-list">
           <div data-id="${note.id}" class="saved-notes">
             <h2 class="title-saved-notes">${escapeHtml(note.title)}</h2>
             <p class="content-saved-notes">
@@ -44,17 +50,22 @@ function createNote(filteredInput = null) {
               "de-DE"
             )}</em></span>
           </div>
+           </div>
     
     `;
   });
 
   notesListEL.innerHTML = sidebar;
+
   applyListeners();
 }
 
 function clickSaveButton() {
   const title = titleInputEl.value;
   const content = noteInputEl.value;
+
+  titleAndInputEl.style.display = "none";
+  sidebarEL.style.display = "flex";
 
   if (!title || !content) {
     alert("bitte Überschrift und Notiz eingeben");
@@ -75,10 +86,13 @@ function clickSaveButton() {
 
   createNote();
   applyListeners();
+  emptyEl.remove();
 }
 
 function selectNote(id) {
+  switchSidebar();
   const notes = getNotes();
+
   const currentNote = notes.find((note) => note.id === parseInt(id));
 
   if (!currentNote) {
@@ -102,10 +116,13 @@ function newNote() {
   titleInputEl.value = "";
   noteInputEl.value = "";
   removeSelectedNoteClass();
+  switchSidebar();
 }
 
 function deleteNote() {
   const notes = getNotes();
+  titleAndInputEl.style.display = "none";
+  sidebarEL.style.display = "flex";
 
   const selectedNoteId = document.querySelector(".selected");
 
@@ -119,6 +136,7 @@ function deleteNote() {
   selectedNoteId.remove();
   titleInputEl.value = "";
   noteInputEl.value = "";
+  updateEmptyNotes();
 }
 
 function removeSelectedNoteClass() {
@@ -128,16 +146,45 @@ function removeSelectedNoteClass() {
   });
 }
 
-function searchNotes(){
-const notes = getNotes();
+function searchNotes() {
+  const notes = getNotes();
 
-const userInput = searchInputEl.value.toLowerCase();
-const filteredInput = notes.filter(note => note.content.toLowerCase().includes(userInput) || 
-note.title.toLowerCase().includes(userInput));
+  const userInput = searchInputEl.value.toLowerCase();
+  const filteredInput = notes.filter(
+    (note) =>
+      note.content.toLowerCase().includes(userInput) ||
+      note.title.toLowerCase().includes(userInput)
+  );
 
-createNote(filteredInput);
+  createNote(filteredInput);
+}
 
+function cancelButton() {
+  titleAndInputEl.style.display = "none";
+  sidebarEL.style.display = "flex";
+  removeSelectedNoteClass();
+}
 
+function switchSidebar() {
+  sidebarEL.style.display = "none";
+  titleAndInputEl.style.display = "block";
+}
 
+function deletAll() {
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
 
+  const savedNotes = document.querySelectorAll(".saved-notes");
+  savedNotes.forEach((notes) => notes.remove());
+
+  titleInputEl.value = "";
+  noteInputEl.value = "";
+  updateEmptyNotes();
+}
+
+function updateEmptyNotes() {
+  const notesAfterDelte = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (!notesAfterDelte || notesAfterDelte.length === 0) {
+    document.querySelector(".sidebar").appendChild(emptyEl);
+  }
 }
